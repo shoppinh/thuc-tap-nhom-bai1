b96f9e89816c381d17f811aa3be08515c0a6a72c
Nguyễn Lộc <loc3120@gmail.com>
2021-01-25 14:34:18 +0700

    Sua file

---
+++ Giao dien/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace Giao_dien
+{
+    partial class frmDangNhap
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txbTenDangNhap = new System.Windows.Forms.TextBox();
+            this.txbMatKhau = new System.Windows.Forms.TextBox();
+            this.lbTenTruyCap = new System.Windows.Forms.Label();
+            this.lbMatKhau = new System.Windows.Forms.Label();
+            this.btnDangNhap = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.btnDangKy = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txbTenDangNhap
+            // 
+            this.txbTenDangNhap.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbTenDangNhap.Location = new System.Drawing.Point(375, 175);
+            this.txbTenDangNhap.Name = "txbTenDangNhap";
+            this.txbTenDangNhap.Size = new System.Drawing.Size(214, 24);
+            this.txbTenDangNhap.TabIndex = 0;
+            // 
+            // txbMatKhau
+            // 
+            this.txbMatKhau.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbMatKhau.Location = new System.Drawing.Point(375, 272);
+            this.txbMatKhau.Name = "txbMatKhau";
+            this.txbMatKhau.PasswordChar = '*';
+            this.txbMatKhau.Size = new System.Drawing.Size(214, 24);
+            this.txbMatKhau.TabIndex = 1;
+            // 
+            // lbTenTruyCap
+            // 
+            this.lbTenTruyCap.AutoSize = true;
+            this.lbTenTruyCap.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTenTruyCap.Location = new System.Drawing.Point(216, 175);
+            this.lbTenTruyCap.Name = "lbTenTruyCap";
+            this.lbTenTruyCap.Size = new System.Drawing.Size(89, 18);
+            this.lbTenTruyCap.TabIndex = 1;
+            this.lbTenTruyCap.Text = "Tên truy cập";
+            // 
+            // lbMatKhau
+            // 
+            this.lbMatKhau.AutoSize = true;
+            this.lbMatKhau.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbMatKhau.Location = new System.Drawing.Point(216, 272);
+            this.lbMatKhau.Name = "lbMatKhau";
+            this.lbMatKhau.Size = new System.Drawing.Size(69, 18);
+            this.lbMatKhau.TabIndex = 1;
+            this.lbMatKhau.Text = "Mật khẩu";
+            // 
+            // btnDangNhap
+            // 
+            this.btnDangNhap.Location = new System.Drawing.Point(260, 387);
+            this.btnDangNhap.Name = "btnDangNhap";
+            this.btnDangNhap.Size = new System.Drawing.Size(127, 29);
+            this.btnDangNhap.TabIndex = 2;
+            this.btnDangNhap.Text = "Đăng nhập";
+            this.btnDangNhap.UseVisualStyleBackColor = true;
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Location = new System.Drawing.Point(494, 387);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(127, 29);
+            this.btnThoat.TabIndex = 3;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            // 
+            // btnDangKy
+            // 
+            this.btnDangKy.Location = new System.Drawing.Point(775, 12);
+            this.btnDangKy.Name = "btnDangKy";
+            this.btnDangKy.Size = new System.Drawing.Size(127, 29);
+            this.btnDangKy.TabIndex = 4;
+            this.btnDangKy.Text = "Đăng ký";
+            this.btnDangKy.UseVisualStyleBackColor = true;
+            // 
+            // frmDangNhap
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1006, 636);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.btnDangKy);
+            this.Controls.Add(this.btnDangNhap);
+            this.Controls.Add(this.lbMatKhau);
+            this.Controls.Add(this.lbTenTruyCap);
+            this.Controls.Add(this.txbMatKhau);
+            this.Controls.Add(this.txbTenDangNhap);
+            this.Name = "frmDangNhap";
+            this.Text = "Đăng nhập";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txbTenDangNhap;
+        private System.Windows.Forms.TextBox txbMatKhau;
+        private System.Windows.Forms.Label lbTenTruyCap;
+        private System.Windows.Forms.Label lbMatKhau;
+        private System.Windows.Forms.Button btnDangNhap;
+        private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.Button btnDangKy;
+    }
+}
+
