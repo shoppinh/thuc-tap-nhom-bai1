4e0bafcc318fca233c1b262d4dee920fcf1106a6
Nguyễn Lộc <loc3120@gmail.com>
2021-01-26 15:13:17 +0700

    Sửa lại tìm kiếm

--- Giao dien/QuanLy.Designer.cs
+++ Giao dien/QuanLy.Designer.cs
@@ -35,14 +35,15 @@
             this.columnDiaChi = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.columnGioiTinh = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.columnLuong = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.columnMaPB = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.btnThem = new System.Windows.Forms.Button();
             this.btnSua = new System.Windows.Forms.Button();
             this.btnXoa = new System.Windows.Forms.Button();
             this.txbTimKiem = new System.Windows.Forms.TextBox();
             this.btnTimKiem = new System.Windows.Forms.Button();
             this.comboTimKiem = new System.Windows.Forms.ComboBox();
-            this.columnMaPB = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.btn2Thoat = new System.Windows.Forms.Button();
+            this.columnTenPB = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -56,11 +57,12 @@
             this.columnDiaChi,
             this.columnGioiTinh,
             this.columnLuong,
-            this.columnMaPB});
+            this.columnMaPB,
+            this.columnTenPB});
             this.dataGridView1.Location = new System.Drawing.Point(58, 129);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.Size = new System.Drawing.Size(928, 334);
+            this.dataGridView1.Size = new System.Drawing.Size(1053, 334);
             this.dataGridView1.TabIndex = 0;
             // 
             // columnMaNV
@@ -104,6 +106,13 @@
             this.columnLuong.MinimumWidth = 6;
             this.columnLuong.Name = "columnLuong";
             this.columnLuong.Width = 125;
+            // 
+            // columnMaPB
+            // 
+            this.columnMaPB.HeaderText = "Mã PB";
+            this.columnMaPB.MinimumWidth = 6;
+            this.columnMaPB.Name = "columnMaPB";
+            this.columnMaPB.Width = 125;
             // 
             // btnThem
             // 
@@ -156,25 +165,16 @@
             this.comboTimKiem.Items.AddRange(new object[] {
             "Mã nhân viên",
             "Tên nhân viên",
-            "Ngày sinh",
-            "Địa chỉ",
-            "Giới tính",
-            "Lương",
-            "Mã PB"});
+            "Mã phòng ban",
+            "Tên phòng ban"});
             this.comboTimKiem.Location = new System.Drawing.Point(58, 52);
             this.comboTimKiem.Name = "comboTimKiem";
             this.comboTimKiem.Size = new System.Drawing.Size(121, 24);
             this.comboTimKiem.TabIndex = 4;
             // 
-            // columnMaPB
-            // 
-            this.columnMaPB.HeaderText = "Mã PB";
-            this.columnMaPB.MinimumWidth = 6;
-            this.columnMaPB.Name = "columnMaPB";
-            this.columnMaPB.Width = 125;
-            // 
             // btn2Thoat
             // 
+            this.btn2Thoat.DialogResult = System.Windows.Forms.DialogResult.Cancel;
             this.btn2Thoat.Location = new System.Drawing.Point(987, 51);
             this.btn2Thoat.Name = "btn2Thoat";
             this.btn2Thoat.Size = new System.Drawing.Size(87, 24);
@@ -183,12 +183,19 @@
             this.btn2Thoat.UseVisualStyleBackColor = true;
             this.btn2Thoat.Click += new System.EventHandler(this.btn2Thoat_Click);
             // 
+            // columnTenPB
+            // 
+            this.columnTenPB.HeaderText = "Tên PB";
+            this.columnTenPB.MinimumWidth = 6;
+            this.columnTenPB.Name = "columnTenPB";
+            this.columnTenPB.Width = 125;
+            // 
             // frmQuanLy
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.btn2Thoat;
-            this.ClientSize = new System.Drawing.Size(1133, 680);
+            this.ClientSize = new System.Drawing.Size(1234, 738);
             this.Controls.Add(this.comboTimKiem);
             this.Controls.Add(this.txbTimKiem);
             this.Controls.Add(this.btnXoa);
@@ -223,5 +230,6 @@
         private System.Windows.Forms.ComboBox comboTimKiem;
         private System.Windows.Forms.DataGridViewTextBoxColumn columnMaPB;
         private System.Windows.Forms.Button btn2Thoat;
+        private System.Windows.Forms.DataGridViewTextBoxColumn columnTenPB;
     }
 }
