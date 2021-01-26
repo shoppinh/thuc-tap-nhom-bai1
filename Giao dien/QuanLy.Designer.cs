c0a6a2270d20ade907c3cebe8c6302554732ba8e
fcmoonsqrt2711 <70206893+fcmoonsqrt2711@users.noreply.github.com>
2021-01-26 12:43:05 +0700

    moi nhat

--- Giao dien/QuanLy.Designer.cs
+++ Giao dien/QuanLy.Designer.cs
@@ -35,13 +35,13 @@
             this.columnDiaChi = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.columnGioiTinh = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.columnLuong = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.columnMaPB = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.btnThem = new System.Windows.Forms.Button();
             this.btnSua = new System.Windows.Forms.Button();
             this.btnXoa = new System.Windows.Forms.Button();
             this.txbTimKiem = new System.Windows.Forms.TextBox();
             this.btnTimKiem = new System.Windows.Forms.Button();
             this.comboTimKiem = new System.Windows.Forms.ComboBox();
-            this.columnMaPB = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.btn2Thoat = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
@@ -57,10 +57,11 @@
             this.columnGioiTinh,
             this.columnLuong,
             this.columnMaPB});
-            this.dataGridView1.Location = new System.Drawing.Point(58, 129);
+            this.dataGridView1.Location = new System.Drawing.Point(44, 113);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.Size = new System.Drawing.Size(928, 334);
+            this.dataGridView1.Size = new System.Drawing.Size(929, 292);
             this.dataGridView1.TabIndex = 0;
             // 
             // columnMaNV
@@ -105,47 +106,59 @@
             this.columnLuong.Name = "columnLuong";
             this.columnLuong.Width = 125;
             // 
+            // columnMaPB
+            // 
+            this.columnMaPB.HeaderText = "Mã PB";
+            this.columnMaPB.MinimumWidth = 6;
+            this.columnMaPB.Name = "columnMaPB";
+            this.columnMaPB.Width = 125;
+            // 
             // btnThem
             // 
-            this.btnThem.Location = new System.Drawing.Point(131, 527);
+            this.btnThem.Location = new System.Drawing.Point(161, 461);
+            this.btnThem.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.btnThem.Name = "btnThem";
-            this.btnThem.Size = new System.Drawing.Size(97, 44);
+            this.btnThem.Size = new System.Drawing.Size(73, 38);
             this.btnThem.TabIndex = 1;
             this.btnThem.Text = "Thêm";
             this.btnThem.UseVisualStyleBackColor = true;
             // 
             // btnSua
             // 
-            this.btnSua.Location = new System.Drawing.Point(447, 527);
+            this.btnSua.Location = new System.Drawing.Point(484, 461);
+            this.btnSua.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.btnSua.Name = "btnSua";
-            this.btnSua.Size = new System.Drawing.Size(97, 44);
+            this.btnSua.Size = new System.Drawing.Size(73, 38);
             this.btnSua.TabIndex = 1;
             this.btnSua.Text = "Sửa";
             this.btnSua.UseVisualStyleBackColor = true;
             // 
             // btnXoa
             // 
-            this.btnXoa.Location = new System.Drawing.Point(799, 527);
+            this.btnXoa.Location = new System.Drawing.Point(795, 461);
+            this.btnXoa.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.btnXoa.Name = "btnXoa";
-            this.btnXoa.Size = new System.Drawing.Size(97, 44);
+            this.btnXoa.Size = new System.Drawing.Size(73, 38);
             this.btnXoa.TabIndex = 1;
             this.btnXoa.Text = "Xóa";
             this.btnXoa.UseVisualStyleBackColor = true;
             // 
             // txbTimKiem
             // 
-            this.txbTimKiem.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txbTimKiem.Location = new System.Drawing.Point(225, 52);
+            this.txbTimKiem.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbTimKiem.Location = new System.Drawing.Point(169, 46);
+            this.txbTimKiem.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.txbTimKiem.Name = "txbTimKiem";
-            this.txbTimKiem.Size = new System.Drawing.Size(420, 24);
+            this.txbTimKiem.Size = new System.Drawing.Size(316, 21);
             this.txbTimKiem.TabIndex = 3;
             this.txbTimKiem.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txbTimKiem_KeyPress);
             // 
             // btnTimKiem
             // 
-            this.btnTimKiem.Location = new System.Drawing.Point(702, 52);
+            this.btnTimKiem.Location = new System.Drawing.Point(526, 46);
+            this.btnTimKiem.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.btnTimKiem.Name = "btnTimKiem";
-            this.btnTimKiem.Size = new System.Drawing.Size(87, 24);
+            this.btnTimKiem.Size = new System.Drawing.Size(65, 21);
             this.btnTimKiem.TabIndex = 1;
             this.btnTimKiem.Text = "Tìm kiếm";
             this.btnTimKiem.UseVisualStyleBackColor = true;
@@ -161,23 +174,19 @@
             "Giới tính",
             "Lương",
             "Mã PB"});
-            this.comboTimKiem.Location = new System.Drawing.Point(58, 52);
+            this.comboTimKiem.Location = new System.Drawing.Point(44, 46);
+            this.comboTimKiem.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.comboTimKiem.Name = "comboTimKiem";
-            this.comboTimKiem.Size = new System.Drawing.Size(121, 24);
+            this.comboTimKiem.Size = new System.Drawing.Size(92, 22);
             this.comboTimKiem.TabIndex = 4;
-            // 
-            // columnMaPB
-            // 
-            this.columnMaPB.HeaderText = "Mã PB";
-            this.columnMaPB.MinimumWidth = 6;
-            this.columnMaPB.Name = "columnMaPB";
-            this.columnMaPB.Width = 125;
             // 
             // btn2Thoat
             // 
-            this.btn2Thoat.Location = new System.Drawing.Point(987, 51);
+            this.btn2Thoat.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btn2Thoat.Location = new System.Drawing.Point(740, 45);
+            this.btn2Thoat.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.btn2Thoat.Name = "btn2Thoat";
-            this.btn2Thoat.Size = new System.Drawing.Size(87, 24);
+            this.btn2Thoat.Size = new System.Drawing.Size(65, 21);
             this.btn2Thoat.TabIndex = 1;
             this.btn2Thoat.Text = "Thoát";
             this.btn2Thoat.UseVisualStyleBackColor = true;
@@ -185,10 +194,10 @@
             // 
             // frmQuanLy
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 14F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.btn2Thoat;
-            this.ClientSize = new System.Drawing.Size(1133, 680);
+            this.ClientSize = new System.Drawing.Size(1028, 595);
             this.Controls.Add(this.comboTimKiem);
             this.Controls.Add(this.txbTimKiem);
             this.Controls.Add(this.btnXoa);
@@ -197,6 +206,7 @@
             this.Controls.Add(this.btnTimKiem);
             this.Controls.Add(this.btnThem);
             this.Controls.Add(this.dataGridView1);
+            this.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.Name = "frmQuanLy";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "QuanLy";
