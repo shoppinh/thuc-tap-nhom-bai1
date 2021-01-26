a2391b9558dfc81dbf0cef197a64172c3971984e
Kienneik <mactrungkien2000@gmail.com>
2021-01-26 13:25:29 +0700

    kien check giao dien

--- Giao dien/Form1.Designer.cs
+++ Giao dien/Form1.Designer.cs
@@ -40,51 +40,46 @@
             // 
             // txbTenTruyCap
             // 
-            this.txbTenTruyCap.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txbTenTruyCap.Location = new System.Drawing.Point(281, 153);
-            this.txbTenTruyCap.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.txbTenTruyCap.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbTenTruyCap.Location = new System.Drawing.Point(375, 175);
             this.txbTenTruyCap.Name = "txbTenTruyCap";
-            this.txbTenTruyCap.Size = new System.Drawing.Size(162, 21);
+            this.txbTenTruyCap.Size = new System.Drawing.Size(214, 24);
             this.txbTenTruyCap.TabIndex = 0;
             // 
             // txbMatKhau
             // 
-            this.txbMatKhau.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txbMatKhau.Location = new System.Drawing.Point(281, 238);
-            this.txbMatKhau.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.txbMatKhau.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbMatKhau.Location = new System.Drawing.Point(375, 272);
             this.txbMatKhau.Name = "txbMatKhau";
             this.txbMatKhau.PasswordChar = '*';
-            this.txbMatKhau.Size = new System.Drawing.Size(162, 21);
+            this.txbMatKhau.Size = new System.Drawing.Size(214, 24);
             this.txbMatKhau.TabIndex = 1;
             // 
             // lbTenTruyCap
             // 
             this.lbTenTruyCap.AutoSize = true;
-            this.lbTenTruyCap.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbTenTruyCap.Location = new System.Drawing.Point(162, 153);
-            this.lbTenTruyCap.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbTenTruyCap.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTenTruyCap.Location = new System.Drawing.Point(216, 175);
             this.lbTenTruyCap.Name = "lbTenTruyCap";
-            this.lbTenTruyCap.Size = new System.Drawing.Size(73, 15);
+            this.lbTenTruyCap.Size = new System.Drawing.Size(89, 18);
             this.lbTenTruyCap.TabIndex = 1;
             this.lbTenTruyCap.Text = "Tên truy cập";
             // 
             // lbMatKhau
             // 
             this.lbMatKhau.AutoSize = true;
-            this.lbMatKhau.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbMatKhau.Location = new System.Drawing.Point(162, 238);
-            this.lbMatKhau.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbMatKhau.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbMatKhau.Location = new System.Drawing.Point(216, 272);
             this.lbMatKhau.Name = "lbMatKhau";
-            this.lbMatKhau.Size = new System.Drawing.Size(56, 15);
+            this.lbMatKhau.Size = new System.Drawing.Size(69, 18);
             this.lbMatKhau.TabIndex = 1;
             this.lbMatKhau.Text = "Mật khẩu";
             // 
             // btnDangNhap
             // 
-            this.btnDangNhap.Location = new System.Drawing.Point(195, 339);
-            this.btnDangNhap.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.btnDangNhap.Location = new System.Drawing.Point(260, 387);
             this.btnDangNhap.Name = "btnDangNhap";
-            this.btnDangNhap.Size = new System.Drawing.Size(95, 25);
+            this.btnDangNhap.Size = new System.Drawing.Size(127, 29);
             this.btnDangNhap.TabIndex = 2;
             this.btnDangNhap.Text = "Đăng nhập";
             this.btnDangNhap.UseVisualStyleBackColor = true;
@@ -92,11 +87,9 @@
             // 
             // btnThoat
             // 
-            this.btnThoat.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.btnThoat.Location = new System.Drawing.Point(370, 339);
-            this.btnThoat.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.btnThoat.Location = new System.Drawing.Point(494, 387);
             this.btnThoat.Name = "btnThoat";
-            this.btnThoat.Size = new System.Drawing.Size(95, 25);
+            this.btnThoat.Size = new System.Drawing.Size(127, 29);
             this.btnThoat.TabIndex = 3;
             this.btnThoat.Text = "Thoát";
             this.btnThoat.UseVisualStyleBackColor = true;
@@ -105,11 +98,10 @@
             // lilbHuongDan
             // 
             this.lilbHuongDan.AutoSize = true;
-            this.lilbHuongDan.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lilbHuongDan.Location = new System.Drawing.Point(633, 21);
-            this.lilbHuongDan.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lilbHuongDan.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lilbHuongDan.Location = new System.Drawing.Point(844, 24);
             this.lilbHuongDan.Name = "lilbHuongDan";
-            this.lilbHuongDan.Size = new System.Drawing.Size(38, 15);
+            this.lilbHuongDan.Size = new System.Drawing.Size(46, 18);
             this.lilbHuongDan.TabIndex = 5;
             this.lilbHuongDan.TabStop = true;
             this.lilbHuongDan.Text = "About";
@@ -118,11 +110,10 @@
             // lilbTaiKhoan
             // 
             this.lilbTaiKhoan.AutoSize = true;
-            this.lilbTaiKhoan.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lilbTaiKhoan.Location = new System.Drawing.Point(539, 21);
-            this.lilbTaiKhoan.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lilbTaiKhoan.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lilbTaiKhoan.Location = new System.Drawing.Point(719, 24);
             this.lilbTaiKhoan.Name = "lilbTaiKhoan";
-            this.lilbTaiKhoan.Size = new System.Drawing.Size(61, 15);
+            this.lilbTaiKhoan.Size = new System.Drawing.Size(73, 18);
             this.lilbTaiKhoan.TabIndex = 6;
             this.lilbTaiKhoan.TabStop = true;
             this.lilbTaiKhoan.Text = "Tài khoản";
@@ -131,10 +122,10 @@
             // frmDangNhap
             // 
             this.AcceptButton = this.btnDangNhap;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 14F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.btnThoat;
-            this.ClientSize = new System.Drawing.Size(920, 462);
+            this.ClientSize = new System.Drawing.Size(1006, 636);
             this.Controls.Add(this.lilbTaiKhoan);
             this.Controls.Add(this.lilbHuongDan);
             this.Controls.Add(this.btnThoat);
@@ -143,7 +134,6 @@
             this.Controls.Add(this.lbTenTruyCap);
             this.Controls.Add(this.txbMatKhau);
             this.Controls.Add(this.txbTenTruyCap);
-            this.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.Name = "frmDangNhap";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Đăng nhập";
